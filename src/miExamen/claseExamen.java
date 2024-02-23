ff1a1f62c3198b77664f59ba2eb26facaa8e72fe
noelrd52 <noelrd52@educastur.es>
2024-02-23 18:57:30 +0000

    Añadidos los primeros ficheros al proyecto

---
+++ src/miExamen/claseExamen.java
@@ -0,0 +1,11 @@
+
+package miExamen;
+
+public class claseExamen {
+
+    public static void main(String[] args) {
+    
+        System.out.println("Estoy haciendo el examen de la UT3");
+    }
+    
+}

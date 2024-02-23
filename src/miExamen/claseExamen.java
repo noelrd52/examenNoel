f63dd60000b4367f5894ff404cfaf4cab37aa03f
noelrd52 <noelrd52@educastur.es>
2024-02-23 19:23:00 +0000

    Los comentarios dentro de /** los trasladé a dentro del void main, fue un error mio

--- src/miExamen/claseExamen.java
+++ src/miExamen/claseExamen.java
@@ -1,18 +1,19 @@
 
 package miExamen;
 
-/**
+
+public class claseExamen {
+
+    public static void main(String[] args) {
+ /**
  * <h1>Entornos de desarrollo UT3</h1>
  * <p>Esta clase sirve para realizar el examen de la UT3
  * de entornos de Desarrollo</p>
- * @see <a href="https://www.youtube.com/playlist?list=PLU8oAlHdN5BlyaPFiNQcV0xDqy0eR35aU></a>
+ * @see <a href="https://www.youtube.com/playlist?list=PLU8oAlHdN5BlyaPFiNQcV0xDqy0eR35aU"></a>
  * @author Noel Roman de Benito
  * @version 1.0.0
  * @since 23/02/2024
  */
-public class claseExamen {
-
-    public static void main(String[] args) {
     
         System.out.println("Estoy haciendo el examen de la UT3");
     }

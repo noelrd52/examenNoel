53f74be784d45175b3cfddb789a79253572d044a
noelrd52 <noelrd52@educastur.es>
2024-02-23 19:06:04 +0000

    Añadidos comentarios de documentacion de Word

--- src/miExamen/claseExamen.java
+++ src/miExamen/claseExamen.java
@@ -1,6 +1,15 @@
 
 package miExamen;
 
+/**
+ * <h1>Entornos de desarrollo UT3</h1>
+ * <p>Esta clase sirve para realizar el examen de la UT3
+ * de entornos de Desarrollo</p>
+ * @see <a href="https://www.youtube.com/playlist?list=PLU8oAlHdN5BlyaPFiNQcV0xDqy0eR35aU></a>
+ * @author Noel Roman de Benito
+ * @version 1.0.0
+ * @since 23/02/2024
+ */
 public class claseExamen {
 
     public static void main(String[] args) {
